f6a796eda5ce6fe541e5842adfeeeab677a982b5
Hyeman-Samuel <Hyeman1738@gmail.com>
2025-02-02 18:48:08 +0100

    Feat: SeedProducts

--- greencart_backend/src/ports/controllers/products/product.ts
+++ greencart_backend/src/ports/controllers/products/product.ts
@@ -16,19 +16,19 @@ export class ProductController {
     ) { }
 
 
-    // @Post('/')
-    // async load(@Res() res: Response ){
-    //     let products = SeededProducts.map(async x=> await this.productService.createProduct({
-    //         title:x.NAME,
-    //         type:x.TYPE,
-    //         carbon_emission:x.CARBON_EMISSIONS,
-    //         price:x.MARKET_PRICE,
-    //         category:x.CATEGORIES as Category,
-    //         quantity:x.QUANTITY !== ""? parseFloat(x.QUANTITY.toString()) : undefined,
-    //         metric:x.MERIC !== ""? x.MERIC as Metric : undefined
-    //     }))
-    //     return res.status(201).json({ products })
-    // }
+    @Post('/')
+    async load(@Res() res: Response ){
+        let products = SeededProducts.map(async x=> await this.productService.createProduct({
+            title:x.NAME,
+            type:x.TYPE,
+            carbon_emission:x.CARBON_EMISSIONS,
+            price:x.MARKET_PRICE,
+            category:x.CATEGORIES as Category,
+            quantity:x.QUANTITY !== ""? parseFloat(x.QUANTITY.toString()) : undefined,
+            metric:x.MERIC !== ""? x.MERIC as Metric : undefined
+        }))
+        return res.status(201).json({ products })
+    }
 
     @Get('/')
     async get(@Res() res: Response,@QueryParam("category") category: string,@QueryParam("offset") offset: number = 0,@QueryParam("limit") limit: number = 15){
